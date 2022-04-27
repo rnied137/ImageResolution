50099202fb6c4b16ab1b0d5cec721dc2c62037ac
Szelma <raduniek1@gmail.com>
2022-04-27 13:09:55 +0200

    Add files via upload

---
+++ ConsoleApp9/ImageDimension.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Utilities
+{
+    public class ImageSize
+    {
+        public ImageSize (long width, long height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public long Width { get; set; }
+        public long Height { get; set; }
+    }
+    // largely credited to https://stackoverflow.com/a/112711/3838199 for the image-specific code
+    // edited by @Radek
+    public static class ImageUtilities
+    {
+        private const string ErrorMessage = "Could not read image data";
+        private static readonly Dictionary<byte[], Func<BinaryReader, ImageSize>> ImageFormatDecoders = new Dictionary<byte[], Func<BinaryReader, ImageSize>>()
+        {
+            { new byte[]{ 0x42, 0x4D }, DecodeBitmap},
+            { new byte[]{ 0x47, 0x49, 0x46, 0x38, 0x37, 0x61 }, DecodeGif },
+            { new byte[]{ 0x47, 0x49, 0x46, 0x38, 0x39, 0x61 }, DecodeGif },
+            { new byte[]{ 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }, DecodePng },
+            { new byte[]{ 0xff, 0xd8 }, DecodeJfif }
+        };
+
+        /// <summary>
+        /// Retrieve the dimensions of an online image, downloading as little as possible
+        /// </summary>
+        public static ImageSize GetSizefromImage(byte[] file)
+        {
+            try
+            {
+            return GetDimensions(new BinaryReader(new MemoryStream(file)));
+            }
+            catch
+            {
+                return new ImageSize(0, 0);
+            }   
+        }
+
+        /// <summary>
+        /// Gets the dimensions of an image.
+        /// </summary>
+        /// <returns>The dimensions of the specified image.</returns>
+        /// <exception cref="ArgumentException">The image was of an unrecognized format.</exception>    
+        public static ImageSize GetDimensions(BinaryReader binaryReader)
+        {
+            int maxMagicBytesLength = ImageFormatDecoders.Keys.OrderByDescending(x => x.Length).First().Length;
+
+            byte[] magicBytes = new byte[maxMagicBytesLength];
+
+            for (int i = 0; i < maxMagicBytesLength; i += 1)
+            {
+                magicBytes[i] = binaryReader.ReadByte();
+
+                foreach (var kvPair in ImageFormatDecoders)
+                {
+                    if (magicBytes.StartsWith(kvPair.Key))
+                    {
+                        return kvPair.Value(binaryReader);
+                    }
+                }
+            }
+
+            throw new ArgumentException(ErrorMessage, nameof(binaryReader));
+        }
+
+        // from https://stackoverflow.com/a/415839/3838199
+
+        private static bool StartsWith(this byte[] thisBytes, byte[] thatBytes)
+        {
+            for (int i = 0; i < thatBytes.Length; i += 1)
+            {
+                if (thisBytes[i] != thatBytes[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static short ReadLittleEndianInt16(this BinaryReader binaryReader)
+        {
+            byte[] bytes = new byte[sizeof(short)];
+            for (int i = 0; i < sizeof(short); i += 1)
+            {
+                bytes[sizeof(short) - 1 - i] = binaryReader.ReadByte();
+            }
+            return BitConverter.ToInt16(bytes, 0);
+        }
+
+        private static int ReadLittleEndianInt32(this BinaryReader binaryReader)
+        {
+            byte[] bytes = new byte[sizeof(int)];
+            for (int i = 0; i < sizeof(int); i += 1)
+            {
+                bytes[sizeof(int) - 1 - i] = binaryReader.ReadByte();
+            }
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        private static ImageSize DecodeBitmap(BinaryReader binaryReader)
+        {
+            binaryReader.ReadBytes(16);
+            int width = binaryReader.ReadInt32();
+            int height = binaryReader.ReadInt32();
+            return new ImageSize(width, height);
+        }
+
+        private static ImageSize DecodeGif(BinaryReader binaryReader)
+        {
+            int width = binaryReader.ReadInt16();
+            int height = binaryReader.ReadInt16();
+            return new ImageSize(width, height);
+        }
+
+        private static ImageSize DecodePng(BinaryReader binaryReader)
+        {
+            binaryReader.ReadBytes(8);
+            int width = binaryReader.ReadLittleEndianInt32();
+            int height = binaryReader.ReadLittleEndianInt32();
+            return new ImageSize(width, height);
+        }
+
+        private static ImageSize DecodeJfif(BinaryReader binaryReader)
+        {
+            while (binaryReader.ReadByte() == 0xff)
+            {
+                byte marker = binaryReader.ReadByte();
+                short chunkLength = binaryReader.ReadLittleEndianInt16();
+
+                if (marker == 0xc0 || marker == 0xc1 || marker == 0xc2)
+                {
+                    binaryReader.ReadByte();
+
+                    int height = binaryReader.ReadLittleEndianInt16();
+                    int width = binaryReader.ReadLittleEndianInt16();
+                    return new ImageSize(width, height);
+                }
+
+                binaryReader.ReadBytes(chunkLength - 2);
+            }
+
+            throw new ArgumentException(ErrorMessage);
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Utilities;
+
+namespace ConsoleApp9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var file = File.ReadAllBytes("C:\\Users\\Szelma\\Pictures\\ooo.jpg");
+            var k = ImageUtilities.GetSizefromImage(file);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(k.Height.ToString());
+            Console.WriteLine(k.Width.ToString());
+
+            file = File.ReadAllBytes("C:\\Users\\Szelma\\Pictures\\image.png");
+             k = ImageUtilities.GetSizefromImage(file);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(k.Height.ToString());
+            Console.WriteLine(k.Width.ToString());
+
+            file = File.ReadAllBytes("C:\\Users\\Szelma\\Pictures\\fff.png");
+
+            k = ImageUtilities.GetSizefromImage(file);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(k.Height.ToString());
+            Console.WriteLine(k.Width.ToString());
+
+            file = File.ReadAllBytes("C:\\Users\\Szelma\\Pictures\\aaa.png");
+
+            k = ImageUtilities.GetSizefromImage(file);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(k.Height.ToString());
+            Console.WriteLine(k.Width.ToString());
+
+        }
+    }
+}
